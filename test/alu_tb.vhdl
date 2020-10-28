5602792d7ea23b452e522874c5a3e396991ab0f6
Patrick Flynn <patrickf4664@protonmail.com>
2020-10-28 10:18:57 -0400

    Re-designed testbench for a wider range of values

--- test/alu_tb.vhdl
+++ test/alu_tb.vhdl
@@ -1,5 +1,6 @@
 library ieee;
 use ieee.std_logic_1164.all;
+use ieee.numeric_std.all;
 use ieee.std_logic_signed.all;
 use ieee.std_logic_arith.all;
 use std.textio.all;
@@ -9,97 +10,112 @@ end alu_tb;
 
 architecture behavior of alu_tb is
     -- Constants
-    constant bus_size : integer := 8;
+    constant BUS_SIZE : integer := 8;
+    constant tb_upper : integer := 20;
+    constant tb_lower : integer := -20;
 
     -- Declare our component
     component AL_UNIT
         port (
-            A, B : in std_logic_vector(bus_size - 1 downto 0);
+            A, B : in std_logic_vector(BUS_SIZE - 1 downto 0);
             OP : in std_logic_vector(3 downto 0);
-            F : out std_logic_vector(bus_size - 1 downto 0)
+            F : out std_logic_vector(BUS_SIZE - 1 downto 0)
         );
     end component;
     
     -- Bind to the component
-    for ALU_0: AL_UNIT use entity work.AL_UNIT;
-    signal A, B, F : std_logic_vector(7 downto 0);
+    for uut: AL_UNIT use entity work.AL_UNIT;
+    signal A, B, F : std_logic_vector(BUS_SIZE - 1 downto 0);
     signal OP : std_logic_vector(3 downto 0);
 begin
     -- Initialize component
-    ALU_0: AL_UNIT port map(A => A, B => B, OP => OP, F => F);
+    uut: AL_UNIT port map(A => A, B => B, OP => OP, F => F);
     
+    -- The process
     process
-        type pattern_type is record
-            A, B : integer;
-            F : integer;
-            OP : std_logic_vector(3 downto 0);
-        end record;
-        
-        -- Put your patterns here
-        type pattern_array is array (natural range <>) of pattern_type;
-        constant patterns: pattern_array := (
-            -- Add tests
-            ( 4, 5, 9, "0000" ),
-            ( 8, 3, 11, "0000" ),
-            -- Subtract tests
-            ( 10, 5, 5, "0001" ),
-            ( 7, 3, 4, "0001" ),
-            ( 3, 11, -8, "0001" ),
-            -- AND tests
-            ( 1, 1, 1, "0100" ),
-            ( 0, 1, 0, "0100" ),
-            ( 7, 4, 4, "0100" ),
-            ( 100, 43, 32, "0100" ),
-            -- OR tests
-            ( 0, 0, 0, "0101" ),
-            ( 1, 0, 1, "0101" ),
-            ( 1, 1, 1, "0101" ),
-            ( 3, 4, 7, "0101" ),
-            ( 8, 7, 15, "0101" ),
-            -- XOR tests
-            ( 1, 1, 0, "0110" ),
-            ( 1, 4, 5, "0110" ),
-            ( 127, 1, 126, "0110" ),
-            -- NOT tests
-            ( 1, 0, -2, "0111" ),
-            ( 4, 0, -5, "0111" ),
-            ( -3, 0, 2, "0111" ),
-            -- 2's complement
-            ( 1, 0, -1, "1000" ),
-            -- Increment
-            ( 1, 0, 2, "1001" ),
-            ( -1, 0, 0, "1001" ),
-            ( 10, 0, 11, "1001" ),
-            -- Decrement
-            ( 1, 0, 0, "1010" ),
-            ( 0, 0, -1, "1010" ),
-            ( 10, 0, 9, "1010" )
-            );
-            
-        variable l : line;
+        variable num1_slv, num2_slv : std_logic_vector(BUS_SIZE - 1 downto 0);
     begin
-        wait for 100 ns;
-        
-        -- Verify each pattern
-        for i in patterns'range loop
-            A <= conv_std_logic_vector(patterns(i).A, bus_size);
-            B <= conv_std_logic_vector(patterns(i).B, bus_size);
-            OP <= patterns(i).OP;
-            wait for 100 ns;
-            
-            -- Debug code
-            --write(l, String'("Result: "));
-            --write(l, to_bitvector(F));
-            --writeline(output, l);
-        
-            assert F = patterns(i).F report "Test failed." severity error;
+    
+        -- Test the two-operand functions
+        for i in tb_lower to tb_upper loop
+            for j in tb_lower to tb_upper loop
+                num1_slv := std_logic_vector(to_signed(i, BUS_SIZE));
+                num2_slv := std_logic_vector(to_signed(j, BUS_SIZE));
+                
+                A <= num1_slv;
+                B <= num2_slv;
+                
+                -- Addition
+                OP <= "0000";
+                wait for 20 ns;
+                
+                assert conv_integer(F) = (i + j) report "Test failed-> Add" severity error;
+                
+                -- Subtraction
+                OP <= "0001";
+                wait for 20 ns;
+                
+                assert conv_integer(F) = (i - j) report "Test failed-> Sub" severity error;
+                
+                -- AND
+                OP <= "0100";
+                wait for 20 ns;
+                
+                assert conv_integer(F) = conv_integer(num1_slv and num2_slv) report "Test failed-> And" severity error;
+                
+                -- OR
+                OP <= "0101";
+                wait for 20 ns;
+                
+                assert conv_integer(F) = conv_integer(num1_slv or num2_slv) report "Test failed-> OR" severity error;
+                
+                -- XOR
+                OP <= "0110";
+                wait for 20 ns;
+                
+                assert conv_integer(F) = conv_integer(num1_slv xor num2_slv) report "Test failed- XOR" severity error;
+            end loop;
         end loop;
         
+        wait for 100 ns;
+        
+        -- Test the one-operand functions
+        for i in tb_lower to tb_upper loop
+            num1_slv := std_logic_vector(to_signed(i, BUS_SIZE));
+            A <= num1_slv;
+            
+            -- NOT
+            OP <= "0111";
+            wait for 20 ns;
+            
+            assert conv_integer(F) = conv_integer(not num1_slv) report "Test failed-> NOT" severity error;
+            
+            -- 2's complement
+            OP <= "1000";
+            wait for 20 ns;
+            
+            assert conv_integer(F) = (-i) report "Test failed-> 2's complement" severity error;
+            
+            -- Increment
+            OP <= "1001";
+            wait for 20 ns;
+            
+            assert conv_integer(F) = (i + 1) report "Test failed-> INC" severity error;
+            
+            -- Decrement
+            OP <= "1010";
+            wait for 20 ns;
+            
+            assert conv_integer(F) = (i - 1) report "Test failed-> DEC" severity error;
+        end loop;
+        
+        wait for 100 ns;
+        
+        -- Finish up
         assert false report "" severity note;
         assert false report "Test completed." severity note;
         assert false report "" severity note;
         wait;
-        
     end process;
 end architecture;
 
